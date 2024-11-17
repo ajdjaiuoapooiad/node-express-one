e2cdf107735af44220f53b5c7f33d8505a23cd1b
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-17 10:11:31 +0900

    ok

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 var express = require('express');
 
 app = express();
+
+
+app.get('/',function(req,res){
+    res.send('Hello Express');
+})
+
+app.post('/about',function(req,res){
+    res.send('Hello About');
+})
+
+app.get('/contact',function(req,res){
+    res.send('Hello Contact');
+})
+
+
+app.listen(8000,function(){
+    console.log('Server run success');
+    
+})
